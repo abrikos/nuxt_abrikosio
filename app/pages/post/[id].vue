9e1c96922b4cd0ad72e63f41a457835d4beeada2
Artem Filippow <a.filippov@qtech.ru>
2026-01-22 18:04:51 +0900

    desc

--- app/pages/post/[id].vue
+++ app/pages/post/[id].vue
@@ -40,6 +40,7 @@ onMounted(load)
       Title {{ post.title }}
       Meta(name="og:title" content="My Amazing Site")
       Meta(name="og:description" content="My Amazing Site")
+      Meta(name="description" content="My Amazing Site")
       Meta(name="og:image" :content="post.poster")
       Meta(name="og:url" content="https://abrikosio.ru/post/1")
       Meta(name="og:type" content="article")
