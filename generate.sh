32e6aeed69e1adb19c38d29fbc91626ac5f6d4c4
Artem Filippow <a.filippov@qtech.ru>
2026-01-29 16:15:04 +0900

    script for generate

---
+++ generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nuxt generate
+mv /home/abrikos/PycharmProjects/abrikosio/nuxt_static/public/index.html /home/abrikos/PycharmProjects/abrikosio/templates/
+#mv /home/abrikos/PycharmProjects/abrikosio/nuxt_static/public/* /home/abrikos/PycharmProjects/abrikosio/static/
